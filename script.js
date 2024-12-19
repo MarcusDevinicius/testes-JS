2ca8c7f08aad8b96189d565e52fdf7f223b0be89
Marcus Dev <163601890+MarcusDevinicius@users.noreply.github.com>
2024-12-19 11:24:23 -0300

    tudo certo

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const listaGames = document.querySelectorAll('.lista-games li')
 const listaSections = document.querySelectorAll('.lista-sections section')
 
 function relacionaImg(index) {
+    listaSections.forEach((section) => {
+        section.classList.remove('ativo')
+    })
     listaSections[index].classList.add('ativo')
 }
 
 listaGames.forEach((game, index) => {
-    game[index]
-    game.addEventListener('click', relacionaImg)
+    game.addEventListener('click', () => {
+        relacionaImg(index)
+    })
     
 })
+
+const gamesFav = document.querySelector('dt')
+const listaGamesFav = document.querySelector('.lista-games')
+
+function callBackList() {
+    gamesFav.classList.toggle('ativo')
+    listaGamesFav.classList.toggle('ativo')
+}
+
+gamesFav.addEventListener('click', callBackList)
